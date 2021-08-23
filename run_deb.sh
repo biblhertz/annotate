44fc34e26516dfe4c640f6c17cf0376b61b405e5
Elisa Bastianello <liladude@aruba.it>
2021-08-23 11:55:13 +0200

    Create run_deb.sh
    Try to have a working run.sh on debian 10

---
+++ run_deb.sh
@@ -0,0 +1,16 @@
+bundle install
+bundle update
+bundle exec jekyll build -d _site
+wait
+ln -s $PWD/assets/images/custom $PWD/_data
+bundle exec jekyll serve --force_polling --host='0.0.0.0' --config _config.yml,_config_dev.yml --port='5555' &
+python3 -m pip install --upgrade --user pip
+python3 -m venv ENV
+#python pyvenv ENV
+echo source ENV/bin/activate
+cd _site/assets/python
+python3 -m pip install --user Flask
+export FLASK_APP=flaskserver.py
+python -m flask run --host=0.0.0.0 --port=5000
+pkill -f jekyll
+© 2021 GitHub, Inc.
